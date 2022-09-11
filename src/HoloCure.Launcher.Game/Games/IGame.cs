bc4668779e592e4afc9c1fb40cebd428ff5786c0
Tomat <xxlennygamerxx@gmail.com>
2022-09-10 20:31:23 -0700

    guh???

---
+++ src/HoloCure.Launcher.Game/Games/IGame.cs
@@ -0,0 +1,24 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+
+namespace HoloCure.Launcher.Game.Games
+{
+    // TODO: tabs
+    // TODO: profiles
+    /// <summary>
+    ///     A game that may be launched by this launcher.
+    /// </summary>
+    public interface IGame
+    {
+        SpriteText MakeMainWindowTitle();
+
+        Drawable MakeSiderbarIcon();
+
+        Drawable MakeSidebarTitle();
+
+        /// <summary>
+        ///     Converts this object to a <see cref="Drawable"/>.
+        /// </summary>
+        Drawable AsDrawable();
+    }
+}
