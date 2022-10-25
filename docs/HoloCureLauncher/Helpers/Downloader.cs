d04794e5a438f6af01376fb6b7a1629663471a55
Tomat <xxlennygamerxx@gmail.com>
2022-10-25 16:55:58 -0700

    Begin documentation of Downloader.

--- docs/HoloCureLauncher/Helpers/Downloader.cs
+++ docs/HoloCureLauncher/Helpers/Downloader.cs
@@ -1,5 +1,7 @@
 ﻿#define TRACE // This is defined in the actual source file
 
+using System.Configuration;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -26,62 +28,62 @@ namespace HoloCureLauncher.Helpers;
 public class Downloader
 {
     /// <summary>
-    /// 
+    ///     The directory that the game should be downloaded to. Initialized using <see cref="ConfigurationManager.AppSettings"/><c>["GameDirectory"]</c>.
     /// </summary>
-    private static string GameDirectory; // = ConfigurationManager.AppSettings["GameDirectory"];
+    private static string GameDirectory;
 
     /// <summary>
-    /// 
+    ///     The itch.io API key. Initialized using <see cref="ConfigurationManager.AppSettings"/><c>["ApiKey"]</c>.
     /// </summary>
-    private static string ApiKey; // = ConfigurationManager.AppSettings["ApiKey"];
+    private static string ApiKey;
 
     /// <summary>
-    /// 
+    ///     The itch.io download key. Initialized using <see cref="ConfigurationManager.AppSettings"/><c>["DownloadKey"]</c>.
     /// </summary>
-    private static string DownloadKey; // = ConfigurationManager.AppSettings["DownloadKey"];
+    private static string DownloadKey;
 
     /// <summary>
-    /// 
+    ///     Supposedly the itch.io download URL; goes unused. Initialized using <see cref="ConfigurationManager.AppSettings"/><c>["GameUrl"]</c>.
     /// </summary>
-    private static string GameUrl; // = ConfigurationManager.AppSettings["GameUrl"];
+    private static string GameUrl;
 
     /// <summary>
-    /// 
+    ///     The itch.io game ID. Set in <see cref="GetGameId"/>.
     /// </summary>
     private static string GameId;
 
     /// <summary>
-    /// 
+    ///     The itch.io download ID. Set in <see cref="GetVersions"/>.
     /// </summary>
     private static string DownloadId;
 
     /// <summary>
-    /// 
+    ///     The value <see cref="Directory.GetCurrentDirectory"/> returns in the constructor.
     /// </summary>
     private static string RootPath;
 
     /// <summary>
-    /// 
+    ///     The path created from combining <see cref="RootPath"/> and <see cref="GameDirectory"/>.
     /// </summary>
     private static string GameDirectoryPath;
 
     /// <summary>
-    /// 
+    ///     The path created from combining <see cref="GameDirectoryPath"/> and <c>"version.ini"</c>.
     /// </summary>
     private static string VersionFilePath;
 
     /// <summary>
-    /// 
+    ///     The online <c>updated_at</c> timestamp. Set in <see cref="GetVersions"/>.
     /// </summary>
     private static string OnlineVersion;
 
     /// <summary>
-    /// 
+    ///     The client to make HTTP requests with. Initialized with the default, paramterless constructor.
     /// </summary>
-    private HttpClient HttpClient; // = new HttpClient();
+    private HttpClient HttpClient;
 
     /// <summary>
-    /// 
+    ///     Initializes <see cref="RootPath"/>, <see cref="GameDirectoryPath"/>, and <see cref="VersionFilePath"/>, then invokes <see cref="GetGameId"/> and <see cref="GetVersions"/>.
     /// </summary>
     public Downloader() {
         /* Code omitted. */
