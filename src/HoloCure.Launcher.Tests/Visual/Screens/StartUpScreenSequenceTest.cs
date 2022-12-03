f1bbf15efd6d0c58797dc0b9cdac2d2f3840595b
Tomat <xxlennygamerxx@gmail.com>
2022-12-03 12:55:47 -0800

    Better start-up sequence tests.

--- src/HoloCure.Launcher.Tests/Visual/Screens/StartUpScreenSequenceTest.cs
+++ src/HoloCure.Launcher.Tests/Visual/Screens/StartUpScreenSequenceTest.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Tomat. Licensed under the GPL v3 License.
 // See the LICENSE-GPL file in the repository root for full license text.
 
-using System;
 using HoloCure.Launcher.Base.Graphics.UI.Screens;
 using NUnit.Framework;
 using osu.Framework.Allocation;
@@ -13,6 +12,10 @@ namespace HoloCure.Launcher.Game.Tests.Visual.Screens;
 [TestFixture]
 public class StartUpScreenSequenceTest : LauncherTestScene
 {
+    private double timePerAction = 250D;
+
+    protected override double TimePerAction => timePerAction;
+
     [BackgroundDependencyLoader]
     private void load()
     {
@@ -28,13 +31,6 @@ public class StartUpScreenSequenceTest : LauncherTestScene
 
         AddStep("push startup screen", () => stack.Push(screen));
         AddStep("play default sequence", () => screen.RunDefaultIntroSequence(() => sequenceCompleted = true));
-
-        //AddDurationWaitStep("wait for reveal", StartUpScreen.DEFAULT_REVEAL_DURATION);
-        AddWaitStep("wait for reveal", (int)Math.Round(StartUpScreen.DEFAULT_REVEAL_DURATION / TimePerAction, MidpointRounding.AwayFromZero));
-        AddAssert("assert sequence not completed", () => !sequenceCompleted);
-
-        //AddDurationWaitStep("wait for hide", StartUpScreen.DEFAULT_HIDE_DURATION);
-        AddWaitStep("wait for hide", (int)Math.Round((StartUpScreen.DEFAULT_HIDE_TIME_UNTIL_RUN - StartUpScreen.DEFAULT_REVEAL_DURATION) / TimePerAction, MidpointRounding.AwayFromZero) + 1);
-        AddAssert("assert sequence completed", () => sequenceCompleted);
+        AddUntilStep("wait until sequence completed", () => sequenceCompleted);
     }
 }
